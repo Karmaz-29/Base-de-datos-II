4527a4696c282e50f43a2a9e7a5ce8fe029442b4
Karol Castro <karolcastroor0305@gmail.com>
2024-07-27 20:05:34 -0600

    Conexion transacciones_1

---
+++ transacciones_1/src/main/java/transacciones/transacciones_1/bdMySQL/Conexion.java
@@ -0,0 +1,37 @@
+package transacciones.transacciones_1bdMySQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author karol
+ */
+public class Conexion {
+    private static Connection conexion;
+    
+    public Connection obtenerConexion() {
+        if (conexion == null) {
+            try {
+                conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/transacciones_1?serverTimezone=UTC", "root","");
+                System.out.println("Conexión establecida correctamente.");
+            } catch (SQLException e) {
+                System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+            }
+        }
+        return conexion;
+    }
+
+    // Método para cerrar la conexión a la base de datos
+    public void cerrarConexion() {
+        if (conexion != null) {
+            try {
+                conexion.close();
+                System.out.println("Conexión cerrada correctamente.");
+            } catch (SQLException e) {
+                System.out.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+    }
+}
